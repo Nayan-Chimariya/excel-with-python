fe2f4b8da2adfa0a597d027bae96d1a38a108b22
Nayan <cnayan789@gmail.com>
2022-05-03 10:40:00 +0545

    user section completed

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ import os
 wb = load_workbook('bank.xlsx')
 ws = wb.active
 
-def does_uid_exist(UID):
+def does_value_exist(value,col1,col2):
   count = 0
-  uid = UID
-  for row in range(1,21):
-    for col in range(1,2):
+  uid = value
+  for row in range(1,23):
+    for col in range(col1,col2):
       char = get_column_letter(col)
-      if(ws[char + str(row)].value) == uid:
+      if (ws[char + str(row)].value) == uid:
         cell_row = row
         count +=1
         break
+    if (ws[char + str(row)].value) == uid:
+      break
   if count == 1:
+    count = 0
     return cell_row
   else:
     return False
@@ -71,40 +74,51 @@ def commandList(account_type):
       "-------------------------\n")
 
 
-def Deposit(cell_row):
-  os.system("cls")
-  deposit_amt = int(input("How much would you like to deposit?: "))
+def Deposit(cell_row,type,transferred):
+  if type == "deposit":
+    os.system("cls")
+    deposit_amt = int(input("How much would you like to deposit?: "))
+  elif type == "transfer":
+    deposit_amt = transferred
   for row in range(cell_row,cell_row+1):
     for col in range(5,6):
       char = get_column_letter(col)
       previous_bal = ws[char + str(row)].value
       ws[char + str(row)].value = int(previous_bal)+deposit_amt
   new_bal = previous_bal + deposit_amt
-      
-  print(f"Successfully deposited amount {deposit_amt}\n"
-  f"Your new balance is {new_bal}\n")
+
+  if type == "deposit":    
+    print(f"Successfully deposited amount {deposit_amt}\n"
+    f"Your new balance is {new_bal}\n")
   wb.save('bank.xlsx')
-  commandList("general user")
   
-def Withdraw(cell_row):
-  os.system("cls")
+def Withdraw(cell_row,type):
   for row in range(cell_row,cell_row+1):
     for col in range(5,6):
       char = get_column_letter(col)
       previous_bal = ws[char + str(row)].value
-      withdraw_amt = int(input("How much would you like to withdraw?\n"
+      withdraw_amt = int(input(f"How much would you like to {type}?\n"
       f"maximum amount |{previous_bal}|: "))
       if withdraw_amt< previous_bal:
         ws[char + str(row)].value = int(previous_bal)-withdraw_amt
         new_bal = previous_bal - withdraw_amt
+        pronoun = "withdrew" if type == "withdraw" else "transferred"
         print(
-          f"Successfully withdrew amount {withdraw_amt}\n"
+          f"Successfully {pronoun} amount {withdraw_amt}\n"
           f"Your new balance is {new_bal}\n")
       else:
-        print("You dont have enough balance for the withdrawl")
+        print(f"You dont have enough balance for the {type}")
 
   wb.save('bank.xlsx')
-  commandList("general user")
+  return withdraw_amt
+
+def Transfer(cell_row):
+  receiver = int(input("Enter account number of the receiver: "))
+  receiver_acc_no = does_value_exist(receiver,2,3)
+  if receiver_acc_no != False:
+    transfer = Withdraw(cell_row,"transfer")
+    Deposit(receiver_acc_no,"transfer",transfer)
+    
 
 def available_function(account_type,cell_row):
   if account_type == "general user":
@@ -112,26 +126,27 @@ def available_function(account_type,cell_row):
     while True:
       command = input("command number: ")
       if command == "1":
-        Deposit(cell_row)
+        Deposit(cell_row,"deposit",0)
+        commandList("general user")
       elif command == "2":
-        Withdraw(cell_row)
+        os.system("cls")
+        Withdraw(cell_row,"withdraw")
+        commandList("general user")
       elif command == "3":
-        print("command 3")
+        os.system("cls")
+        Transfer(cell_row)
+        commandList("general user")
       elif command == "4":
         os.system("cls")
         print("User details\n")
         account_details(cell_row)
         commandList("general user")
-
       else:  
         break
 
   else:
     commandList("admin")
     
-
-
-
 print(
   "---------------\n"
   "Welcome to bank\n"
@@ -140,26 +155,24 @@ print(
 
 acount_type = " "
 
-uid = int(input("Enter you ID: "))
-cell_row = (does_uid_exist(uid))
-if cell_row != False:
-  pin =input("Enter pin: ")
-  if (pin_validity(pin,cell_row)) == "valid":
-    print("\nUser details")
-    account_type = account_details(cell_row)
-    available_function(account_type,cell_row)
-    print("test")
+def main():
+  uid = int(input("Enter you ID: "))
+  cell_row = (does_value_exist(uid,1,2))
+  if cell_row != False:
+    pin =input("Enter pin: ")
+    if (pin_validity(pin,cell_row)) == "valid":
+      print("\nUser details")
+      account_type = account_details(cell_row)
+      available_function(account_type,cell_row)
+      is_continue = input("Do you want to continue? Y/N: ").lower()
+      if(is_continue == 'y'):
+        os.system("cls")
+        main()
 
-else:
-  print("UID doesnt exists in the databse\n"
-    "create new account or try another UID\n")
 
+  else:
+    print("UID doesnt exists in the databse\n"
+      "create new account or try another UID\n")
 
-#work in progress 
-'''acc_no = int(input("Enter you account number: "))
+main()
 
-#checking through UID column in excel sheet for account type
-
-for row in range(1,21):
-  for col in range(1,2):
-    char = get_column_letter(col)'''
